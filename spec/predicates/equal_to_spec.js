6548f04914332af145c6ab3d23ddfe450bc7c9c9
Nathan Sobo <grockit@Macintosh.local>
2008-12-01 17:43:06 -0800

    Implemented Predicates.EqualTo#evaluate. Needs more spec coverage

--- spec/predicates/equal_to_spec.js
+++ spec/predicates/equal_to_spec.js
@@ -1,37 +1,31 @@
 require("/specs/june_spec_helper");
 
-//Screw.Unit(function() {
-//  describe("Predicates.EqualTo", function() {
-//    var selection, operand, predicate;
-//
-//    before(function() {
-//      operand = User;
-//      predicate = new Predicates.EqualTo()
-//    });
-//
-//    describe("#evaluate", function() {
-//      describe("when #operand_1 is an Attribute and #operand_2 is a constant", function() {
-//        describe("when passed a Tuple with a field value for the Attribute that =='s the constant", function() {
-//
-//          before(function() {
-//          });
-//
-//
-//          it("returns true", function() {
-//
-//          });
-//        });
-//
+Screw.Unit(function() {
+  describe("Predicates.EqualTo", function() {
+    var predicate;
+
+    describe("#evaluate", function() {
+      describe("when #operand_1 is an Attribute and #operand_2 is a constant", function() {
+        describe("when passed a Tuple with a field value for the Attribute that =='s the constant", function() {
+          before(function() {
+            predicate = new Predicates.EqualTo(User.first_name, "Nathan");
+          });
+
+          it("returns true", function() {
+            expect(predicate.evaluate(User.create({first_name: "Nathan"}))).to(equal, true);
+          });
+        });
+
 //        describe("when passed a Tuple with a field value for the Attribute that !='s the constant", function() {
 //
 //        });
 //
-//      });
-//
+      });
+
 //      describe("when #operand_2 is an Attribute and #operand_1 is a constant", function() {
 //
 //      });
-//    });
-//
-//  });
-//});
+    });
+
+  });
+});
