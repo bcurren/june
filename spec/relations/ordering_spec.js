0209e68cb1d0c2cf13c378220f603e290bd26c8d
Marcel Molina & Nathan Sobo <grockit@grok-a.local>
2009-05-04 16:01:24 -0700

    Added Ordering#attribute, which changes the ordering attribute and triggers handlers registered with #on_reorder.

--- spec/relations/ordering_spec.js
+++ spec/relations/ordering_spec.js
@@ -3,6 +3,10 @@ require("/specs/june_spec_helper");
 Screw.Unit(function(c) { with(c) {
   describe("Ordering", function() {
     var ordering;
+    before(function() {
+      ordering = new June.Relations.Ordering(User, User.age, 'asc');
+    });
+    
     describe("#all", function() {
       context("when #direction is ascending", function() {
         before(function() {
@@ -45,6 +49,44 @@ Screw.Unit(function(c) { with(c) {
           expect(actual_ids).to(equal, expected_ids);
         });
       });
+    });
+
+    describe("#attribute", function() {
+      var reorder_handler;
+      before(function() {
+        reorder_handler = mock_function("reorder handler", function() {
+          expect(ordering.attribute()).to(equal, User.name);
+        });
+        ordering.on_reorder(reorder_handler);
+      });
+
+
+      context("when called with no arguments", function() {
+        it("returns the ordering attribute", function() {
+          expect(ordering.attribute()).to(equal, ordering._attribute);
+        });
+      });
+
+      context("when called with an Attribute", function() {
+        it("returns the assigned Attribute", function() {
+          expect(ordering.attribute(User.name)).to(equal, User.name);
+        });
+
+        context("when called with an Attribute that is different from the Ordering's current attribute", function() {
+          it("changes the ordering attribute and triggers #on_reorder handlers with the Ordering", function() {
+            ordering.attribute(User.name);
+            expect(reorder_handler).to(have_been_called, with_args(ordering));
+          });
+        });
+
+        context("when called with an Attribute that is the same as the Ordering's current attribute", function() {
+          it("does not trigger #on_reorder handlers", function() {
+            ordering.attribute(ordering.attribute());
+            expect(reorder_handler).to_not(have_been_called);
+          });
+        });
+      });
+
     });
   });
 }});
