95ef673b44f4a38056af4b4756d74226a6257876
Marcel Molina & Nathan Sobo <grockit@grok-a.local>
2009-05-04 16:18:16 -0700

    Ordering#direction changes the ordering direction and triggers #on_reorder handlers.

--- spec/relations/ordering_spec.js
+++ spec/relations/ordering_spec.js
@@ -74,6 +74,7 @@ Screw.Unit(function(c) { with(c) {
 
         context("when called with an Attribute that is different from the Ordering's current attribute", function() {
           it("changes the ordering attribute and triggers #on_reorder handlers with the Ordering", function() {
+            expect(ordering.attribute()).to_not(equal, User.name);
             ordering.attribute(User.name);
             expect(reorder_handler).to(have_been_called, with_args(ordering));
           });
@@ -87,6 +88,43 @@ Screw.Unit(function(c) { with(c) {
         });
       });
 
+    });
+
+    describe("#direction", function() {
+      var reorder_handler;
+      before(function() {
+        reorder_handler = mock_function("reorder handler", function() {
+          expect(ordering.direction()).to(equal, "desc");
+        });
+        ordering.on_reorder(reorder_handler);
+      });
+
+      context("when called with no arguments", function() {
+        it("returns the ordering direction", function() {
+          expect(ordering.direction()).to(equal, ordering._direction);
+        });
+        
+        context("when called with a direction", function() {
+          it("returns the assigned direction", function() {
+            expect(ordering.direction('desc')).to(equal, 'desc');
+          });
+
+          context("when called with a direction that is different from the Ordering's current direction", function() {
+            it("changes the direction and triggers #on_reorder handlers with the Ordering", function() {
+              expect(ordering.direction()).to_not(equal, 'desc');
+              ordering.direction('desc');
+              expect(reorder_handler).to(have_been_called, with_args(ordering));
+            });
+          });
+
+          context("when called with a direction that is the same as the Ordering's current attribute", function() {
+            it("does not trigger #on_reorder handlers", function() {
+              ordering.direction(ordering.direction());
+              expect(reorder_handler).to_not(have_been_called);
+            });
+          });
+        });
+      });
     });
   });
 }});
